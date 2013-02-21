09d98b2662599be68bbb23fc59892ee2337ad8ed
Xananax <xananax@yelostudio.com>
2013-02-21 03:00:34 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Framebox
+
+Library to facilitate communication between embedded iframes and container page
+
+Empty placeholder for a library to come
---
+++ main.js
@@ -0,0 +1,4 @@
+//see
+//http://benvinegar.github.com/seamless-talk/
+//https://developer.mozilla.org/en-US/docs/DOM/window.postMessage
+//http://ejohn.org/blog/cross-window-messaging/
